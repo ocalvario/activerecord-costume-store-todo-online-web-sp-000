5f05ec7625d837713a2dab3ab3a4c51deb69a574
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-11 21:25:17 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,4 @@
-class CreateCostumeStores < ActiveRecord::Migration
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costume_stores do |t|
       t.string :name
