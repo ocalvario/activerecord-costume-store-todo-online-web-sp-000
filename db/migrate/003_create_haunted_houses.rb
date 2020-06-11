d72d75dfb292712649dc1073ca15b86953d3c1c8
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-11 21:33:55 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,4 @@
-lass CreateHauntedHouses < ActiveRecord::Migration[5.1]
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
       t.string :name
